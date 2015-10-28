552182f2b7c34d5fd5898f5646cda303a615929b
Maxime Poulain <bosstamam@gmail.com>
2015-10-28 09:17:00 +0100

    temp

--- anykernel.sh
+++ anykernel.sh
@@ -3,22 +3,21 @@
 
 ## AnyKernel setup
 # EDIFY properties
-kernel.string=DirtyV by bsmitty83 @ xda-developers
+kernel.string=Khaon kernel for nexus 10
 do.devicecheck=1
 do.initd=1
 do.modules=0
-do.cleanup=1
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
+do.cleanup=0
+device.name1=manta
+device.name2=
+device.name3=
 device.name4=
 device.name5=
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/platform/dw_mmc.0/by-name/boot;
 
 ## end setup
-
 
 ## AnyKernel methods (DO NOT CHANGE)
 # set up extracted files and directories
@@ -31,8 +30,13 @@ chmod -R 755 $bin;
 mkdir -p $ramdisk $split_img;
 cd $ramdisk;
 
-OUTFD=`ps | grep -v "grep" | grep -oE "update(.*)" | cut -d" " -f3`;
-ui_print() { echo "ui_print $1" >&$OUTFD; echo "ui_print" >&$OUTFD; }
+OUTFD=$1;
+ui_print() {
+  until [ ! "$1" ]; do
+    echo -e "ui_print $1\nui_print" > $OUTFD;
+    shift;
+  done;
+}
 
 # dump boot and extract ramdisk
 dump_boot() {
@@ -143,13 +147,40 @@ replace_file() {
   chmod $2 $1;
 }
 
+# check partition filesystem
+getfs() { $ramdisk/sbin/bb/busybox blkid $1 | grep -Eo 'TYPE="(.+)"' | $ramdisk/sbin/bb/busybox cut -d\" -f2; }
+
+# patch the fstab accordingly to the current partitions's file systems
+patch_fstab() {
+  bb=$ramdisk/sbin/bb/busybox;
+  cache=/dev/block/platform/dw_mmc.0/by-name/cache;
+  data=/dev/block/platform/dw_mmc.0/by-name/userdata;
+  system=/dev/block/platform/dw_mmc.0/by-name/system;
+  prefix=/dev/block/platform/dw_mmc.0/by-name;
+  device=manta;
+  ui_print "Patching fstab ...";
+  # swap out entries for filesystems as detected
+  for i in $data $cache $system; do
+    fstype=`getfs $i`;
+    fsentry=`$bb grep $i $ramdisk/fstab-$fstype.$device`;
+    if [ "$fsentry" ]; then
+      ui_print "${i#${prefix}}'s file system is $fstype";
+      $bb sed -i "s|^$i.*|$fsentry|" $ramdisk/fstab.$device;
+    fi;
+  done;
+  ui_print "fstab patching done.";
+  ui_print " ";
+  ui_print "If you migrate from ext4 to f2fs or vice versa.";
+  ui_print "You will have to install this package again.";
+  $bb rm -f $ramdisk/fstab-*;
+}
+
 ## end methods
 
 
 ## AnyKernel permissions
 # set permissions for included files
 chmod -R 755 $ramdisk
-chmod 644 $ramdisk/sbin/media_profiles.xml
 
 
 ## AnyKernel install
@@ -157,36 +188,9 @@ dump_boot;
 
 # begin ramdisk changes
 
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
-append_file init.rc "run-parts" init;
-
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0\n";
-append_file init.tuna.rc "dvbootscript" init.tuna;
-
-# init.superuser.rc
-if [ -f init.superuser.rc ]; then
-  backup_file init.superuser.rc;
-  replace_string init.superuser.rc "Superuser su_daemon" "# su daemon" "\n# Superuser su_daemon";
-  prepend_file init.superuser.rc "SuperSU daemonsu" init.superuser;
-else
-  replace_file init.superuser.rc 750 init.superuser.rc;
-  insert_line init.rc "init.superuser.rc" after "on post-fs-data" "    import /init.superuser.rc\n";
-fi;
-
-# fstab.tuna
-backup_file fstab.tuna;
-replace_line fstab.tuna "/by-name/system" "/dev/block/platform/omap/omap_hsmmc.0/by-name/system    /system             ext4      nodev,noatime,nodiratime,barrier=0,data=writeback,noauto_da_alloc,discard    wait";
-replace_line fstab.tuna "/by-name/cache" "/dev/block/platform/omap/omap_hsmmc.0/by-name/cache     /cache              ext4      nosuid,nodev,noatime,nodiratime,errors=panic,barrier=0,nomblk_io_submit,data=writeback,noauto_da_alloc    wait,check";
-replace_line fstab.tuna "/by-name/userdata" "/dev/block/platform/omap/omap_hsmmc.0/by-name/userdata  /data               ext4      nosuid,nodev,noatime,errors=panic,nomblk_io_submit,data=writeback,noauto_da_alloc    wait,check,encryptable=/dev/block/platform/omap/omap_hsmmc.0/by-name/metadata";
-append_file fstab.tuna "usbdisk" fstab;
-
+patch_fstab;
 # end ramdisk changes
 
 write_boot;
 
 ## end install
-

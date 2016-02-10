8dc6c67a4d93a5d1a994d1e221f18be4dc0bdf10
Maxime Poulain <maxime.poulain@outlook.be>
2016-02-10 19:50:19 +0100

    repacking back the ramdisk with gzip to ensure compatiblity with supersu

--- anykernel.sh
+++ anykernel.sh
@@ -102,9 +102,9 @@ write_boot() {
     dtb="--dt $split_img/$dtb";
   fi;
   cd $ramdisk;
-  find . | cpio -H newc -o | $bin/lz4 -l -9 > /tmp/anykernel/ramdisk-new.cpio.lz4;
-  $bin/mkbootimg --kernel /tmp/anykernel/zImage --ramdisk /tmp/anykernel/ramdisk-new.cpio.lz4 $second --cmdline "$cmdline" --board "$board" --base $base --pagesize $pagesize --kernel_offset $kerneloff --ramdisk_offset $ramdiskoff $secondoff --tags_offset $tagsoff $dtb --output /tmp/anykernel/boot-new.img;
-  ui_print " "; ui_print "repacking the ramdisk with lz4 algorithm";
+  find . | cpio -H newc -o | gzip > /tmp/anykernel/ramdisk-new.cpio.gzip;
+  $bin/mkbootimg --kernel /tmp/anykernel/zImage --ramdisk /tmp/anykernel/ramdisk-new.cpio.gzip $second --cmdline "$cmdline" --board "$board" --base $base --pagesize $pagesize --kernel_offset $kerneloff --ramdisk_offset $ramdiskoff $secondoff --tags_offset $tagsoff $dtb --output /tmp/anykernel/boot-new.img;
+  ui_print " "; ui_print "repacking the ramdisk with gzip algorithm";
   if [ $? != 0 -o `wc -c < /tmp/anykernel/boot-new.img` -gt `wc -c < /tmp/anykernel/boot.img` ]; then
     ui_print " "; ui_print "Repacking image failed. Aborting...";
     echo 1 > /tmp/anykernel/exitcode; exit;
